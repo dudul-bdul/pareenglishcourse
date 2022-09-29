fbede12181d83981c0718bb6e92486e9d6b365ba
Anticopyright <anticopyright.dev@gmail.com>
2022-09-29 10:54:39 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -41,7 +41,8 @@ module.exports = function (eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
   eleventyConfig.addPassthroughCopy('src/admin');
-  eleventyConfig.addPassthroughCopy('src/assets/img');
+  // eleventyConfig.addPassthroughCopy('src/assets/img');
+  eleventyConfig.addPassthroughCopy('assets/img');
 
   // Customize Markdown library and settings
   let markdownLibrary = markdownIt({
